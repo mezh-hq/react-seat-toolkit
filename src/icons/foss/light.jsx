a4cace7472eb85a098a6d39659d0ece692d1ac4e
Akalanka <akalankaperera128@gmail.com>
2023-08-27 01:30:17 +0530

    Fix: package exports

--- src/icons/foss/light.jsx
+++ src/icons/foss/light.jsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge";
-import logo from "@/assets/images/logos/foss-light.png";
+import logo from "../../assets/images/logos/foss-light.png";
 
 const FossLight = ({ className, ...props }) => {
   return <img src={logo} alt="FOSS Logo" className={twMerge("w-[74px] h-[42px]", className)} {...props} />;
