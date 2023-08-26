7ddd0640021b83e87e4d760da5a474e04c4d8e9e
Akalanka <akalankaperera128@gmail.com>
2023-08-27 01:55:58 +0530

    Fix: use breakpoint tailwind config import

--- src/hooks/breakpoint.jsx
+++ src/hooks/breakpoint.jsx
@@ -1,6 +1,31 @@
 import { useEffect, useState } from "react";
+import path from "path";
 import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
+
+let tailwindConfig;
+
+let iterations = 0;
+
+// eslint-disable-next-line no-constant-condition
+while (true) {
+  iterations++;
+  if (iterations > 10) {
+    tailwindConfig = {
+      theme: {
+        screens: {
+          xs: "400px",
+          xsm: "450px"
+        }
+      }
+    };
+    break;
+  }
+  try {
+    tailwindConfig = require(`${path.join(process.cwd(), "../".repeat(iterations), "tailwind.config.js")}`);
+    break;
+    // eslint-disable-next-line no-empty
+  } catch (e) {}
+}
 
 const config = resolveConfig(tailwindConfig);
 
