f2f261c5d80786f78cc8fd8995535c92a9ecb6c1
Akalanka <akalankaperera128@gmail.com>
2023-09-04 20:07:08 +0530

    Patch(no-records): text alignment

--- src/components/no-records.jsx
+++ src/components/no-records.jsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 export const NoRecords = ({ text = "No Records Found", ...props }) => {
   return (
     <div className={twMerge(`w-full h-full flex justify-center items-center`, props.className)}>
-      <span className="text-2xl font-semibold">{text}</span>
+      <span className="text-2xl font-semibold text-center">{text}</span>
     </div>
   );
 };
