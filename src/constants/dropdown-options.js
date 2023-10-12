e593770ab97dfb735e23573ddf49db86c28f77cd
Akalanka <akalankaperera128@gmail.com>
2023-10-13 00:45:57 +0530

    Feat: added common dropdown options

---
+++ src/constants/dropdown-options.js
@@ -0,0 +1,37 @@
+export const genderOptions = {
+  key: "gender",
+  label: "Gender",
+  options: [
+    {
+      key: "M",
+      label: "Male"
+    },
+    {
+      key: "F",
+      label: "Female"
+    },
+    {
+      key: "O",
+      label: "Other"
+    },
+    {
+      key: "-",
+      label: "Rather not say"
+    }
+  ]
+};
+
+export const mealPreferenceOptions = {
+  key: "meal_preference",
+  label: "Meal Preference",
+  options: [
+    {
+      key: "VEG",
+      label: "Vegetarian"
+    },
+    {
+      key: "NON_VEG",
+      label: "Non-vegetarian"
+    }
+  ]
+};
