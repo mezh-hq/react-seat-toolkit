8c8339730680db3fa21d88127d99a35b500a5bd2
Akalanka <akalankaperera128@gmail.com>
2023-10-15 00:27:42 +0530

    Feat: added use round hook

---
+++ src/hooks/round.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const rounds = {
+  "ROUND 01": 1,
+  "FINAL": 2
+};
+
+const roundKeys = Object.keys(rounds);
+
+const useRound = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [round, setRound] = useState(searchParams.get("round") || 1);
+  const onRoundChange = (value) => {
+    setRound(rounds[value]);
+    setSearchParams({ round: rounds[value] });
+  };
+  return { rounds: roundKeys, round, roundKey: roundKeys[round - 1], onRoundChange };
+};
+
+export default useRound;
