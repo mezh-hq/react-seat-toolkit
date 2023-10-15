d17e96a231e8235aac9a12cf3f10e55333197d3a
Akalanka <akalankaperera128@gmail.com>
2023-10-15 19:58:58 +0530

    Feat: added ghost icon

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,5 +1,6 @@
 export { default as ArrowPair } from "./arrow-pair";
 export { default as Bashaway } from "./bashaway";
 export { default as FOSS } from "./foss";
+export { default as Ghost } from "./ghost";
 export { default as Times } from "./times";
 export { default as Link } from "./link";
