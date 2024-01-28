3028ae76e2f28fc199f1a2d9f91c5bf3d2dc781a
Akalanka Perera <akalankaperera128@gmail.com>
2024-01-28 16:01:53 +0530

    Feat: migrated twin switch to typescript

--- src/components/core/switch/twin-switch.tsx
+++ src/components/core/switch/twin-switch.tsx
@@ -5,12 +5,19 @@ const padding = "px-4 py-2";
 
 const valueStyles = "z-50 pointer-events-none transition-all duration-medium";
 
-const TwinSwitch = ({ values = [], selectedValue, onChange, handleClassName, ...props }) => {
+interface ITwinSwitchProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "onChange"> {
+  values: string[];
+  selectedValue?: string;
+  onChange?: (value: string) => void;
+  handleClassName?: string;
+}
+
+const TwinSwitch = ({ values = [], selectedValue, onChange, handleClassName, ...props }: ITwinSwitchProps) => {
   const [selected, setSelected] = useState(values[0]);
   const [handleStyles, setHandleStyles] = useState({});
 
-  const value1Ref = useRef();
-  const value2Ref = useRef();
+  const value1Ref = useRef<HTMLSpanElement>();
+  const value2Ref = useRef<HTMLSpanElement>();
 
   useEffect(() => {
     if (selectedValue && selectedValue !== selected) toggle();
