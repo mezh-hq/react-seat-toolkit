3e6d9424de4dbec80ddf311c1dc6a20c1da795d3
Akalanka Perera <akalankaperera128@gmail.com>
2024-02-05 15:37:04 +0530

    Feat: added categorizer component

---
+++ src/components/controls/select/index.jsx
@@ -0,0 +1,11 @@
+import Categorizer from "./categorizer";
+
+const SelectControls = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Categorizer />
+    </div>
+  );
+};
+
+export default SelectControls;
