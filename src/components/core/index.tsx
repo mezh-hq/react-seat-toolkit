f92cc4aa8a1a9b1815a159b1148ae9b09b6e6729
Akalanka Perera <akalankaperera128@gmail.com>
2024-03-12 15:01:24 +0530

    Patch: cleaned dependencies

--- src/components/core/index.tsx
+++ src/components/core/index.tsx
@@ -1,15 +1,11 @@
-export * from "./accordion";
 export * from "./checkbox";
-export * from "./context-menu";
 export * from "./input";
 export * from "./label";
 export * from "./popover";
 export * from "./radio-group";
 export * from "./select";
-export * from "./sheet";
 export * from "./switch";
 export * from "./typography";
-export * from "./toast";
 export * from "./tooltip";
 
 export { default as AnimatedSwitcher } from "./animated-switcher";
