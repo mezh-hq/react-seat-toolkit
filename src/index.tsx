6608af10c376e2bdafeed0946e2c83ca9f0538ef
Akalanka Perera <akalankaperera128@gmail.com>
2024-03-16 17:08:24 +0530

    Fix: exported seat status

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { default as Core, TooltipProvider } from "@/components";
 import { store } from "@/store";
 import { type ISTKProps } from "./types";
 
+export { SeatStatus } from "@/types/elements";
+
 export const SeatToolkit = (props: ISTKProps) => {
   return (
     <Provider store={store}>
