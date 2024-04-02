ec6786990782dc30ed4384fa2504527fd24a3b4a
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-02 20:23:01 +0530

    Fix: workspace load render issue

--- src/hooks/events/workspace-load.ts
+++ src/hooks/events/workspace-load.ts
@@ -10,38 +10,46 @@ const useWorkspaceLoad = (props: ISTKProps) => {
   useLayoutEffect(() => {
     if (props.data) {
       store.dispatch(sync(props.data));
-      setTimeout(() => {
-        const {
-          height: workspaceheight,
-          width: workspaceWidth,
-          top: workspaceTop,
-          left: workspaceLeft
-        } = d3Extended.selectionBounds(d3Extended.selectById(ids.workspace));
-        const workspaceGroup = d3Extended.select(selectors.workspaceGroup);
-        const {
-          left: wgOffsetLeft,
-          top: wgOffsetTop,
-          height: workspaceGroupHeight,
-          width: workspaceGroupWidth
-        } = d3Extended.selectionBounds(workspaceGroup);
-        if (workspaceGroupHeight && workspaceGroupWidth) {
-          let scaleFactor = props.data.workspace?.initialViewBoxScale ?? 1;
-          if (props.data.workspace?.visibilityOffset) {
-            workspaceGroup.attr(dataAttributes.visibilityOffset, props.data.workspace.visibilityOffset);
+      (async () => {
+        while (true) {
+          await new Promise((resolve) => setTimeout(resolve, 0));
+          const {
+            height: workspaceheight,
+            width: workspaceWidth,
+            top: workspaceTop,
+            left: workspaceLeft
+          } = d3Extended.selectionBounds(d3Extended.selectById(ids.workspace));
+          const workspaceGroup = d3Extended.select(selectors.workspaceGroup);
+          const {
+            left: wgOffsetLeft,
+            top: wgOffsetTop,
+            height: workspaceGroupHeight,
+            width: workspaceGroupWidth
+          } = d3Extended.selectionBounds(workspaceGroup);
+          if (workspaceGroupHeight && workspaceGroupWidth) {
+            let scaleFactor = props.data.workspace?.initialViewBoxScale ?? 1;
+            if (props.data.workspace?.visibilityOffset) {
+              workspaceGroup.attr(dataAttributes.visibilityOffset, props.data.workspace.visibilityOffset);
+            }
+            scaleFactor *= 1.05;
+            panAndZoom({
+              k: scaleFactor,
+              y:
+                (workspaceheight +
+                  workspaceTop -
+                  (wgOffsetTop * scaleFactor * 2 + workspaceGroupHeight * scaleFactor)) /
+                2,
+              x:
+                (workspaceWidth +
+                  workspaceLeft -
+                  (wgOffsetLeft * scaleFactor * 2 + workspaceGroupWidth * scaleFactor)) /
+                2
+            });
+            store.dispatch(initializeWorkspace());
+            break;
           }
-          scaleFactor *= 1.05;
-          panAndZoom({
-            k: scaleFactor,
-            y:
-              (workspaceheight + workspaceTop - (wgOffsetTop * scaleFactor * 2 + workspaceGroupHeight * scaleFactor)) /
-              2,
-            x:
-              (workspaceWidth + workspaceLeft - (wgOffsetLeft * scaleFactor * 2 + workspaceGroupWidth * scaleFactor)) /
-              2
-          });
-          store.dispatch(initializeWorkspace());
         }
-      }, 0);
+      })();
     } else {
       store.dispatch(initializeElements());
     }
