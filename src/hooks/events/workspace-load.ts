156e083b07a82f32e3730c64fc4d6db862e6ea56
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-02 20:19:59 +0530

    Revert: "Fix: workspace load render issue"
    This reverts commit 2afea063fb70709e652d20757cebab1c29583a4b.

--- src/hooks/events/workspace-load.ts
+++ src/hooks/events/workspace-load.ts
@@ -8,58 +8,44 @@ import { d3Extended } from "@/utils";
 
 const useWorkspaceLoad = (props: ISTKProps) => {
   useLayoutEffect(() => {
-    (async () => {
-      if (props.data) {
-        store.dispatch(sync(props.data));
-        while (true) {
-          const initialized = await new Promise((resolve) => {
-            setTimeout(() => {
-              const {
-                height: workspaceheight,
-                width: workspaceWidth,
-                top: workspaceTop,
-                left: workspaceLeft
-              } = d3Extended.selectionBounds(d3Extended.selectById(ids.workspace));
-              const workspaceGroup = d3Extended.select(selectors.workspaceGroup);
-              const {
-                left: wgOffsetLeft,
-                top: wgOffsetTop,
-                height: workspaceGroupHeight,
-                width: workspaceGroupWidth
-              } = d3Extended.selectionBounds(workspaceGroup);
-              if (workspaceGroupHeight && workspaceGroupWidth) {
-                let scaleFactor = props.data.workspace?.initialViewBoxScale ?? 1;
-                if (props.data.workspace?.visibilityOffset) {
-                  workspaceGroup.attr(dataAttributes.visibilityOffset, props.data.workspace.visibilityOffset);
-                }
-                scaleFactor *= 1.05;
-                panAndZoom({
-                  k: scaleFactor,
-                  y:
-                    (workspaceheight +
-                      workspaceTop -
-                      (wgOffsetTop * scaleFactor * 2 + workspaceGroupHeight * scaleFactor)) /
-                    2,
-                  x:
-                    (workspaceWidth +
-                      workspaceLeft -
-                      (wgOffsetLeft * scaleFactor * 2 + workspaceGroupWidth * scaleFactor)) /
-                    2
-                });
-                store.dispatch(initializeWorkspace());
-                resolve(true);
-              } else {
-                resolve(false);
-              }
-            }, 0);
+    if (props.data) {
+      store.dispatch(sync(props.data));
+      setTimeout(() => {
+        const {
+          height: workspaceheight,
+          width: workspaceWidth,
+          top: workspaceTop,
+          left: workspaceLeft
+        } = d3Extended.selectionBounds(d3Extended.selectById(ids.workspace));
+        const workspaceGroup = d3Extended.select(selectors.workspaceGroup);
+        const {
+          left: wgOffsetLeft,
+          top: wgOffsetTop,
+          height: workspaceGroupHeight,
+          width: workspaceGroupWidth
+        } = d3Extended.selectionBounds(workspaceGroup);
+        if (workspaceGroupHeight && workspaceGroupWidth) {
+          let scaleFactor = props.data.workspace?.initialViewBoxScale ?? 1;
+          if (props.data.workspace?.visibilityOffset) {
+            workspaceGroup.attr(dataAttributes.visibilityOffset, props.data.workspace.visibilityOffset);
+          }
+          scaleFactor *= 1.05;
+          panAndZoom({
+            k: scaleFactor,
+            y:
+              (workspaceheight + workspaceTop - (wgOffsetTop * scaleFactor * 2 + workspaceGroupHeight * scaleFactor)) /
+              2,
+            x:
+              (workspaceWidth + workspaceLeft - (wgOffsetLeft * scaleFactor * 2 + workspaceGroupWidth * scaleFactor)) /
+              2
           });
-          if (initialized) break;
+          store.dispatch(initializeWorkspace());
         }
-      } else {
-        store.dispatch(initializeElements());
-      }
-      props.events?.onWorkspaceLoad?.();
-    })();
+      }, 0);
+    } else {
+      store.dispatch(initializeElements());
+    }
+    props.events?.onWorkspaceLoad?.();
   }, [props.data]);
 };
 
