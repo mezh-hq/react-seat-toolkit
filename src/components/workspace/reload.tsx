76cbdff7d9141490e3da0e9fec634afb57dbbe6c
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-05 21:06:38 +0530

    Fix: reload button icon size

--- src/components/workspace/reload.tsx
+++ src/components/workspace/reload.tsx
@@ -9,12 +9,12 @@ const Reloader = (props: Pick<ISTKProps, "mode" | "styles" | "options">) => {
     <div
       id={ids.reloader}
       className={twMerge(
-        "absolute top-5 right-4 pl-7 flex justify-center items-center cursor-pointer border bg-gray-50 hover:bg-gray-100 [&>svg]:hover:-rotate-45 [&>svg]:transition-all [&>svg]:transition-medium rounded-md p-2 transition-all duration-medium",
+        "absolute top-4 right-4 pl-7 flex justify-center items-center cursor-pointer border bg-gray-50 hover:bg-gray-100 [&>svg]:hover:-rotate-45 [&>svg]:transition-all [&>svg]:transition-medium rounded-md p-2 transition-all duration-medium",
         props.styles?.reloadButton?.className
       )}
       style={props.styles?.reloadButton?.properties}
     >
-      <RotateCcw />
+      <RotateCcw size={20.5} />
     </div>
   );
 };
