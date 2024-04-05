aa3dccfac2a3bb2b072d47d81d964eb28d846664
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-05 21:47:39 +0530

    Feat: added deselect all elements action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,10 @@
 import { panDown, panLeft, panRight, panUp } from "@/components/workspace/zoom";
 import { store } from "@/store";
-import { deselectElement } from "@/store/reducers/editor";
+import { clearElements, deselectElement } from "@/store/reducers/editor";
 
 export const actions = {
   deselectElement: (elementId: string) => store.dispatch(deselectElement(elementId)),
+  deselectAllElements: () => store.dispatch(clearElements(false)),
   panLeft,
   panDown,
   panRight,
