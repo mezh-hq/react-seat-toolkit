c2d5c329816dfe426cd3ae9a640f308422b69a25
Akalanka Perera <akalankaperera128@gmail.com>
2024-06-18 14:51:27 +0530

    Docs: added logo to storybook header

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-  brandTitle: 'Mezh HQ | Seat Toolkit',
   brandUrl: 'https://mezh-hq.github.io',
+  brandTitle:  `<div style="display:flex;align-items:center;gap:6px;"><img src="./logo.jpg" style="border-radius:8px;" width="45px" height="45px"/><span>Mezh HQ | Seat Toolkit</span></div>`,
   brandTarget: '_self',
 });
