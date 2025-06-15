29df618916fb72235afe76dc7999d9594a2e622b
Akalanka Perera <akalankaperera128@gmail.com>
2025-06-15 12:28:17 +0530

    Fix: ssr build issue

--- src/constants/user-agent.ts
+++ src/constants/user-agent.ts
@@ -1,2 +1,4 @@
-export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-export const isMobileSafari = /iP(ad|hone|od).+Version\/[\d.]+.*Safari/i.test(navigator.userAgent);
+export const isSafari =
+  typeof window !== "undefined" ? /^((?!chrome|android).)*safari/i.test(navigator.userAgent) : false;
+export const isMobileSafari =
+  typeof window !== "undefined" ? /iP(ad|hone|od).+Version\/[\d.]+.*Safari/i.test(navigator.userAgent) : false;
